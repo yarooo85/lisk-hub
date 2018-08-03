55fa463d7f3b0dc52f0767174e992a22fb9eddb6
andreiklimenok <andrei.klimenok@lightcurve.io>
2018-08-03 11:55:26 +0200

    :white_check_mark: Jenkinsfile change 3 - clean project

--- cypress/integration/examples/sample.spec.js
+++
@@ -1,9 +0,0 @@
-describe('My first test', () => {
-  it('Visits the kitchen sink', () => {
-    cy.visit('http://localhost:8080');
-    cy.contains('.passphrase input')
-      .click()
-      .type('huy')
-      .should('have.value', 'fake@email.com')
-  })
-})
